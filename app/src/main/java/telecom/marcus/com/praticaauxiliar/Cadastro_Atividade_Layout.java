2ac867ac5b27d85df7a138d0146b7bb47412061a
MarcusTelecom <marcus.ufsj@gmail.com>
2019-03-22 01:40:27 -0300

    Strings Primeira parte

---
+++ app/src/main/java/telecom/marcus/com/praticaauxiliar/Cadastro_Atividade_Layout.java
@@ -0,0 +1,13 @@
+package telecom.marcus.com.praticaauxiliar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_Atividade_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro__atividade__layout);
+    }
+}
