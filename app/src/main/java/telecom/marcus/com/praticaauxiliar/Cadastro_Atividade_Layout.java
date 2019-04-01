15e0f246d0b438449f3502277fe602a5db89016e
MarcusTelecom <marcus.ufsj@gmail.com>
2019-04-01 14:00:52 -0300

    Adicionado id aos RadioGroups Adicionado botão Salvar

--- app/src/main/java/telecom/marcus/com/praticaauxiliar/Cadastro_Atividade_Layout.java
+++ app/src/main/java/telecom/marcus/com/praticaauxiliar/Cadastro_Atividade_Layout.java
@@ -2,12 +2,22 @@ package telecom.marcus.com.praticaauxiliar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Cadastro_Atividade_Layout extends AppCompatActivity {
+
+    Button btSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro__atividade__layout);
+    }
+
+    public void salvar(View view) {
+        btSalvar = (Button) findViewById(R.id.bt_salvar);
+        Toast.makeText(getApplicationContext(),"ss",Toast.LENGTH_SHORT).show();
     }
 }
